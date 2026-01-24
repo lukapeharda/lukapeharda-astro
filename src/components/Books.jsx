bee9ac62278643376fb5d01d7ce2464b9643c6a8
Luka Peharda <luka.peharda@gmail.com>
2026-01-24 12:54:22 +0100

    Add Unreasonable Hospitality to the list of book recommendations

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -6,7 +6,8 @@ function Books({books}) {
             { books.map((book, index) => (
                 <div className="w-full py-2 px-4" key={ book.data.title }>
                     <h3>
-                        <a className={ `text-white light-mode:text-gray-800 p-1 text-lg border-b border-e6n-${colors[index % 4]} hover:bg-e6n-${colors[index % 4]}` } href={ book.data.url } title={ book.data.title } target="_blank">{ book.data.title } <span className="text-sm text-gray-500">by { book.data.author }</span></a>
+                        <a className={ `text-white light-mode:text-gray-800 p-1 text-lg border-b border-e6n-${colors[index % 4]} hover:bg-e6n-${colors[index % 4]}` } href={ book.data.url } title={ book.data.title } target="_blank">{ book.data.title }</a>
+                        <br /><span className="text-sm text-gray-500">by { book.data.author }</span>
                     </h3>
 
                     <p className="text-gray-100 light-mode:text-gray-600 mt-2">{ book.data.description }</p>
